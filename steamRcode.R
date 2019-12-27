b64c93a13890d2d021a368fd0e6344b43dd86087
Sekian <skillout@hotmail.es>
2019-12-27 20:23:41 +0100

    Visualization

--- steamRcode.R
+++ steamRcode.R
@@ -1,5 +1,16 @@
 steam = read.csv('D:/FIB/MIRI/DAKD/DAKD Project/steam.csv')
 
+lvl = levels(steam$genres)
+dd <- steam
+
+library(splitstackshape)
+dd = concat.split.expanded(dd, "platforms", sep  = ";", type = "character")
+
+dd = concat.split.expanded(dd, "genres", sep  = ";", type = "character")
+dd = concat.split.expanded(dd, "categories", sep  = ";", type = "character")
+dd = concat.split.expanded(dd, "steamspy_tags", sep  = ";", type = "character")
+
+###HIST
 TABLEV1= as.data.frame(with(steam,table(developer)))
 TABLEV1 = TABLEV1[TABLEV1$Freq > 10,]
 TABLEV1 <- TABLEV1[order(-TABLEV1$Freq),]
